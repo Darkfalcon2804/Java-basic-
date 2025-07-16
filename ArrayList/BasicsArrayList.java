48a7cc9d6c290d28d9b21f80a81b4fa7f293437c
Darkfalcon2804 <darkfalcon2804@gmail.com>
2025-07-16 08:48:03 +0530

    interfaces

---
+++ ArrayList/BasicsArrayList.java
@@ -0,0 +1,48 @@
+package ArrayList;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BasicsArrayList {
+    public static void main(String[] args){
+        ArrayList<Integer>list=new ArrayList<Integer>(); //this is the syntax to declare array list there some more let see
+//        ArrayList<String>list2=new ArrayList<String>();
+//        ArrayList<Boolean>list3=new ArrayList<Boolean>();
+        //add element
+        list.add(0);
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+
+        //get element
+        int element=list.get(0);
+        System.out.println("The 0th index element of array list is:"+element);
+
+        //to add element in between .add element add im last but in between we do
+        list.add(1,1);
+                System.out.println(list);
+
+                //set element
+        list.set(0,5);
+            System.out.println(list);
+
+            //Delete element
+        list.remove(3);
+        System.out.println(list);
+
+        //size
+       int size= list.size();
+       System.out.println(size);
+
+        //loops
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+        System.out.println();
+        //Sorting
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
+
+
+//for using array list we need import java.util.ArraayList;
--- Oops/Interfaces.java
+++ Oops/Interfaces.java
@@ -1,4 +1,22 @@
 package Oops;
-
-public class Interfaces {
+interface Animals{
+   public void walk();
+    // Animal(){}  //error because interface dont have constructor
 }
+class Horses implements Animals{
+ public   void walk(){
+    System.out.println("walk on 4 legs");
+    }
+}
+public class Interfaces {
+    public static void main(String[] args){
+        Horses horse=new Horses();
+        horse.walk();
+    }
+}
+
+////Interfaces important point 
+//// =>All the fields in interfaces are public,static and final by default
+////=>All methods are public and abstract by default
+////=>A class that implements an interface must implement all the methods declared in the intefaces
+////=>Interfaces support the functionality of multiple inheritance
